46d57834bccf76da76fef47fe964cf9a1ddbdbba
Jaime Fomperosa <jaime.fompe@gmail.com>
2021-11-30 19:56:51 +0100

    Initial commit

---
+++ src/main/java/es/grise/upm/profundizacion/mocking_ejercicio_1/Client.java
@@ -0,0 +1,9 @@
+package es.grise.upm.profundizacion.mocking_ejercicio_1;
+
+public interface Client {
+
+	void receiveMessage(Message message);
+
+	boolean hasEmail();
+
+}
---
+++ src/main/java/es/grise/upm/profundizacion/mocking_ejercicio_1/ExistingClientException.java
@@ -0,0 +1,10 @@
+package es.grise.upm.profundizacion.mocking_ejercicio_1;
+
+public class ExistingClientException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
