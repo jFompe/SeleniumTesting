cf10c176d900e008fcc4338dfbdc732236ce00dd
Jaime Fomperosa <jaime.fompe@gmail.com>
2021-11-30 20:45:26 +0100

    Removed old classes

--- src/main/java/es/grise/upm/profundizacion/mocking_ejercicio_1/Client.java
+++
@@ -1,9 +0,0 @@
-package es.grise.upm.profundizacion.mocking_ejercicio_1;
-
-public interface Client {
-
-	void receiveMessage(Message message);
-
-	boolean hasEmail();
-
-}
--- src/main/java/es/grise/upm/profundizacion/mocking_ejercicio_1/Message.java
+++
@@ -1,5 +0,0 @@
-package es.grise.upm.profundizacion.mocking_ejercicio_1;
-
-public interface Message {
-
-}
